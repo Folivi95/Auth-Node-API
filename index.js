fa55a22cc776c204c20c2f443d5398fadea7e3c5
Folivi95 <joshua.folivi@gmail.com>
2019-09-25 13:37:25 +0100

    initial commit 3

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ const AuthRoute = require('./routes/auth');
 //Route Middleware
 app.use('/api/user', AuthRoute);
 
+
 app.listen(process.env.PORT || 3000, () => console.log(`Server is up and running on port ${process.env.PORT}`))
