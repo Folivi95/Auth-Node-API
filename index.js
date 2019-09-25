08ce95b93e8176ab63c5ecb1cf57eb2eac84676f
Folivi95 <joshua.folivi@gmail.com>
2019-09-25 13:35:35 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+//Importing AuthRouter
+const AuthRoute = require('./routes/auth');
+
+//Route Middleware
+app.use('/api/user', AuthRoute);
+
+app.listen(process.env.PORT || 3000, () => console.log(`Server is up and running on port ${process.env.PORT}`))
