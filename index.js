263613ddc566246445efd61309a3adea62e3ddec
Folivi95 <joshua.folivi@gmail.com>
2019-09-25 16:51:51 +0100

    added support for express json

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+//Importing Routes
+const AuthRoute = require('./routes/auth');
+
 dotenv.config(); //load .env variables
 
 
@@ -12,8 +15,9 @@ mongoose.connect(process.env.DB_CONNECT,
     { useNewUrlParser: true },
     () => console.log('Connect to Database successfully'));
 
-//Importing AuthRouter
-const AuthRoute = require('./routes/auth');
+
+//Middleware
+app.use(express.json());
 
 //Route Middleware
 app.use('/api/user', AuthRoute);
